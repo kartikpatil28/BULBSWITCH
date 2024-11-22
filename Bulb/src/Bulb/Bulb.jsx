318ab23a2ab957a6e615ad0189e593ef068addaa
kartikpatil28 <kartikpatilwork24@gmail.com>
2024-11-22 18:40:16 +0530

    mobile friendly application

--- Bulb/src/Bulb/Bulb.jsx
+++ Bulb/src/Bulb/Bulb.jsx
@@ -8,18 +8,18 @@ const Bulb = () => {
   };
 
   return (
-    <div className="bg-black min-h-screen flex flex-col justify-center items-center">
-      <h1 className="font-extrabold text-4xl mb-8 text-purple-700">
+    <div className="bg-black min-h-screen flex flex-col justify-center items-center p-4">
+      <h1 className="font-extrabold text-4xl mb-8 text-purple-700 text-center">
         BULB SWITCH
       </h1>
-      <div className="flex items-center">
+      <div className="flex flex-col md:flex-row items-center">
         <button
-          className="p-3 text-sm font-bold text-white bg-blue-500 rounded-lg shadow-lg mr-4 hover:bg-blue-600 transition duration-300"
+          className="p-3 text-sm font-bold text-white bg-blue-500 rounded-lg shadow-lg mb-4 md:mb-0 md:mr-4 hover:bg-blue-600 transition duration-300"
           onClick={toggleBulb}
         >
           ON/OFF
         </button>
-        <div className="relative h-[3px] w-48">
+        <div className="relative h-[3px] w-40 md:w-48 mb-4 md:mb-0">
           <div className={`relative h-full w-full ${isOn ? "bg-blue-500" : "bg-blue-500"}`}>
             {!isOn && (
               <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-red-500">
@@ -35,7 +35,7 @@ const Bulb = () => {
           </div>
         </div>
         <div
-          className={`w-[150px] h-[150px] rounded-full ${
+          className={`w-[100px] h-[100px] md:w-[150px] md:h-[150px] rounded-full ${
             isOn
               ? "bg-yellow-400 shadow-[0_0_400px_yellow] transition-all duration-300"
               : "bg-black"
